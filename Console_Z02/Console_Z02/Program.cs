94031aa763893cd19e99867e9ddbfed5ced4300f
GICK00 <71695624+GICK00@users.noreply.github.com>
2021-11-29 15:22:00 +0300

    Add files via upload

---
+++ Console_Z02/Console_Z02/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Console_Z02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repit = true;
+            while (repit == true)
+            {
+                try
+                {
+                    Console.WriteLine("| Проверка точки на нахождение в заштрихованной области");
+                    Console.WriteLine("| Ведите точку.");
+                    Console.Write("| x = ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("| y = ");
+                    int y = Convert.ToInt32(Console.ReadLine());
+
+                    if (y == -100 || y == (-1) * Math.Abs(x))
+                    {
+                        Console.WriteLine("| На границе");
+                    }
+                    else if (y > -100 && y < 0 && y < (-1) * Math.Abs(x))
+                    {
+                        Console.WriteLine("| Внутри");
+                    }
+                    else
+                    {
+                        Console.WriteLine("| Вне");
+                    }
+
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Попробовать снова? Да / Нет");
+                    Console.Write("| : ");
+                    string repitTxT = Convert.ToString(Console.ReadLine());
+
+                    if (repitTxT == "Да")
+                    {
+                        repit = true;
+                        Console.WriteLine("|---------------------------");
+                    }
+                    else if (repitTxT == "Нет")
+                        repit = false;
+                    else
+                    {
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Некорректный ввод данных!");
+                        repit = false;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Некорректный ввод данных!");
+                    Console.WriteLine("| Попробовать снова? Да / Нет");
+                    Console.Write("| : ");
+                    string repitTxT = Convert.ToString(Console.ReadLine());
+
+                    if (repitTxT == "Да")
+                    {
+                        repit = true;
+                        Console.WriteLine("|---------------------------");
+                    }
+                    else if (repitTxT == "Нет")
+                        repit = false;
+                    else
+                    {
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Некорректный ввод данных!");
+                        repit = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Console_Z02_2/Console_Z02_2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Console_Z02_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repit = true;
+            while(repit == true)
+            {
+                try
+                {
+                    Console.WriteLine("| Нахождения неизвестной даты");
+                    Console.WriteLine("| Введите текущию дату в формате дд.мм.гггг");
+                    Console.Write("| : ");
+                    string data = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("| Введите количество прошедших месяцев.");
+                    Console.Write("| : ");
+                    int mouthPast = Convert.ToInt32(Console.ReadLine());
+
+                    Regex reg = new Regex(@"\d{1,4}");
+                    Match n = reg.Match(data);
+
+                    int day = Convert.ToInt32(Convert.ToString(n));
+                    n = n.NextMatch();
+                    int month = Convert.ToInt32(Convert.ToString(n));
+                    n = n.NextMatch();
+                    int year = Convert.ToInt32(Convert.ToString(n));
+                    n = n.NextMatch();
+
+                    if ((day.ToString().Length == 2 || day.ToString().Length == 1) && (month.ToString().Length == 2 || month.ToString().Length == 1) && (year.ToString().Length == 4) && day > 0 && day <= 31 && month <= 12 && month > 0)
+                    {
+                        while (mouthPast > 0)
+                        {
+                            month--;
+                            if (month <= 0)
+                            {
+                                month = 12;
+                                year--;
+                            }
+                            mouthPast--;
+                        }
+
+                        string TxTmonthLost = "";
+                        switch (month)
+                        {
+                            case 01:
+                                TxTmonthLost = "Января";
+                                break;
+                            case 02:
+                                TxTmonthLost = "Февраля";
+                                break;
+                            case 03:
+                                TxTmonthLost = "Марта";
+                                break;
+                            case 04:
+                                TxTmonthLost = "Апреля";
+                                break;
+                            case 05:
+                                TxTmonthLost = "Мая";
+                                break;
+                            case 06:
+                                TxTmonthLost = "Июня";
+                                break;
+                            case 07:
+                                TxTmonthLost = "Июля";
+                                break;
+                            case 08:
+                                TxTmonthLost = "Августа";
+                                break;
+                            case 09:
+                                TxTmonthLost = "Сентября";
+                                break;
+                            case 10:
+                                TxTmonthLost = "Октября";
+                                break;
+                            case 11:
+                                TxTmonthLost = "Ноября";
+                                break;
+                            case 12:
+                                TxTmonthLost = "Декабря";
+                                break;
+                        }
+                        Console.WriteLine("| Старая дата: {0:00} " + TxTmonthLost + " {1:0000}", day, year);
+                    } 
+                    else
+                    {
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Неверный формат даты!");
+                        Console.WriteLine("| Попробовать снова? Да / Нет");
+                        Console.Write("| : ");
+                        string repitTxT = Convert.ToString(Console.ReadLine());
+
+                        if (repitTxT == "Да")
+                        {
+                            repit = true;
+                            Console.WriteLine("|---------------------------");
+                        }
+                        else if (repitTxT == "Нет")
+                            repit = false;
+                        else
+                        {
+                            Console.WriteLine("|---------------------------");
+                            Console.WriteLine("| Некорректный ввод данных!");
+                            repit = false;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("|---------------------------");
+                    Console.WriteLine("| Некорректный ввод данных!");
+                    Console.WriteLine("| Попробовать снова? Да / Нет");
+                    Console.Write("| : ");
+                    string repitTxT = Convert.ToString(Console.ReadLine());
+
+                    if (repitTxT == "Да")
+                    {
+                        repit = true;
+                        Console.WriteLine("|---------------------------");
+                    }
+                    else if (repitTxT == "Нет")
+                        repit = false;
+                    else
+                    {
+                        Console.WriteLine("|---------------------------");
+                        Console.WriteLine("| Некорректный ввод данных!");
+                        repit = false;
+                    }
+                }
+            }
+        }
+    }
+}
