0600b69eca67ea768f81bb5d36f26c0a4397bce9
GICK00 <71695624+GICK00@users.noreply.github.com>
2021-11-29 19:55:07 +0300

    Add files via upload

---
+++ Console_Z02_4/Console_Z02_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Console_Z02_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("| Не симетричная, |\n| не красивая ;(  |");
+
+            int stroka = 5;
+            int probel = 0;
+            for (int i = 8; i > 3; i--)
+            {   
+                Console.Write("| ");
+                for (int j = 0; j < stroka; j++)
+                {
+                    Console.Write("  " + i);
+                }
+                for (int n = 0; n < probel; n++)
+                {
+                    Console.Write("   ");
+                }
+                stroka--;
+                probel++;
+                Console.WriteLine(" |");
+            }
+        }
+    }
+}
